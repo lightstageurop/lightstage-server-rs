a4091266edc4ba6a0a13d80df683dbe09228b84a
Thomas Aldrian <aldrian.thom@proton.me>
2026-07-04 12:34:28 +0100

    30Hz

--- lsserver/src/config.rs
+++ lsserver/src/config.rs
@@ -3,4 +3,4 @@ use std::time::Duration;
 pub const NUM_ARCS: usize = 12;
 pub const LIGHTS_PER_ARC: usize = 14;
 pub const PDS_SUBNET_BASE: &str = "10.37.211.";
-pub const KINET_REFRESH_RATE_MS: Duration = Duration::from_millis(1_000 / 40); // 40Hz
+pub const KINET_REFRESH_RATE_MS: Duration = Duration::from_millis(1_000 / 30); // 40Hz jitters a bit, idk why
