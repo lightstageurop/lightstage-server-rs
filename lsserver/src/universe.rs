5f3587294c41973b625b768c0a0ff163d2515d01
Thomas Aldrian <aldrian.thom@proton.me>
2026-07-07 13:54:33 +0100

    this can probably be deleted tbh

--- lsserver/src/universe.rs
+++ lsserver/src/universe.rs
@@ -1,14 +1,4 @@
-use std::net::SocketAddr;
-
-use kinetrs::KinetHeader;
-
-
-pub struct KinetPowerSupply {
-    remote_adr: SocketAddr,
-    header: KinetHeader,
-}
-
-pub struct LightStage {
-    rgb_supplies: Vec<KinetPowerSupply>,
-    white_supplies: Vec<KinetPowerSupply>,
-}
+// pub struct LightStage {
+//     rgb_supplies: Vec<KinetPowerSupply>,
+//     white_supplies: Vec<KinetPowerSupply>,
+// }
