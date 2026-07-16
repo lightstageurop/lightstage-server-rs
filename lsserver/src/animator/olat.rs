64b7803c51a271a8d42062390e85d4eb0ff2181f
Thomas Aldrian <aldrian.thom@proton.me>
2026-07-16 16:58:16 +0100

    olat fixes

--- lsserver/src/animator/olat.rs
+++ lsserver/src/animator/olat.rs
@@ -9,6 +9,7 @@ pub struct OlatAnimator {
     current_light: usize,
     num_arcs: usize,
     lights_per_arc: usize,
+    done: bool,
 }
 
 impl OlatAnimator {
@@ -18,12 +19,14 @@ impl OlatAnimator {
             current_light: 0,
             num_arcs: config.num_arcs,
             lights_per_arc: config.lights_per_arc,
+            done: false,
         }
     }
 }
 
 impl Animator for OlatAnimator {
-    fn tick(&mut self, renderer: &mut Renderer) {
+    fn tick(&mut self, renderer: &mut Renderer) -> bool {
+        // clear all fixtures
         for arc in &mut renderer.rgb_fixtures {
             for light in arc {
                 light.set_color(0, 0, 0);
@@ -35,6 +38,19 @@ impl Animator for OlatAnimator {
             }
         }
 
+        // If we finished already, return
+        if self.done {
+            return false;
+        }
+
+        // illuminate the current light
+        renderer.white_fixtures[self.current_arc][self.current_light].set_white(
+            u16::MAX,
+            u16::MAX,
+            u16::MAX,
+        );
+
+        // advance pattern
         self.current_light += 1;
         if self.current_light >= self.lights_per_arc {
             self.current_light = 0;
@@ -42,12 +58,15 @@ impl Animator for OlatAnimator {
         }
         if self.current_arc >= self.num_arcs {
             self.current_arc = 0;
+            self.done = true;
         }
 
-        renderer.white_fixtures[self.current_arc][self.current_light].set_white(
-            u16::MAX,
-            u16::MAX,
-            u16::MAX,
-        );
+        true
+    }
+
+    fn start(&mut self) {
+        self.current_arc = 0;
+        self.current_light = 0;
+        self.done = false;
     }
 }
